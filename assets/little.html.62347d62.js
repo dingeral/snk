b136d63ce6258e4778ee997e3d67ea91b2c0200b
dingeral <a13625816@outlook.com>
2022-11-17 06:52:15 +0000

    Deploying to gh-pages from @ dingeral/snk@416ca9e4379587d360c4b6cb84ecabb171b8a8de 🚀

---
+++ assets/little.html.62347d62.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as a,c as o,a as e,b as r}from"./app.4cb90eef.js";const c={},s=e("h1",{id:"简短有力",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#简短有力","aria-hidden":"true"},"#"),r(" 简短有力")],-1),n=[s];function _(i,l){return a(),o("div",null,n)}const f=t(c,[["render",_],["__file","little.html.vue"]]);export{f as default};
